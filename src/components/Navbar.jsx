fb2c59f995e2ddd06c7657452e50e1c47946e7ad
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-04-27 21:13:55 +0300

    Initialize commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+        <div className='navbar-wrapper'>
+            navbar
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
